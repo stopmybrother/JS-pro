20644dc66aab3d1b43f07ecd857ec939b8b7f921
Zhenya Aleksandrova <laberfo@gmail.com>
2022-01-30 20:07:18 +0300

    changed reduceFromScratch_task2.js

--- reduceFromScratch_task2.js
+++ reduceFromScratch_task2.js
@@ -1,35 +1,20 @@
 // 2) написать reduce с нуля
 // reduceFromScratch, которая принимает в себя функцию или условие, массив, аккумулятор
 
-//1-ый способ
 let array = [1, 2, 3, 4, 5];
 let accumulator = 5;
 
-let reduceAnArray = (arr, accum) => {
+let getSum = (arr) => {
 	let sum = 0;
-
 	arr.forEach((item) => {
 		sum += item;
 	});
-	return accum + sum;
+	return sum;
 };
 
-let reduceFromScratch = (funk, array, accumulator) => {
-	return funk(array, accumulator);
-};
-console.log(reduceFromScratch(reduceAnArray, array, accumulator));
-
-
-//2-ой способ
-let array = [1, 2, 3, 4, 5];
-let accumulator = 5;
-
-let reduceAnArray = (array, accumulator) => {
-	let previousValue = accumulator;
-	let newArr = array.reduce((previousValue, currentValue, index, array) => {
-		return previousValue + currentValue;
-	}, previousValue);
-	return newArr;
+let reduceAnArray = (arr, accum) => {
+	let sum = getSum(arr);
+	return sum + accum;
 };
 
 let reduceFromScratch = (funk, array, accumulator) => {
