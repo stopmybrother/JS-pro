2fc27701126cf79865b509415afac7dd0886f526
Zhenya Aleksandrova <laberfo@gmail.com>
2022-01-30 20:06:35 +0300

    changed mapFromScratch_task1.js

--- mapFromScratch_task1.js
+++ mapFromScratch_task1.js
@@ -3,29 +3,20 @@
 // массив, к каждому элементу которого должна применяться эта функция, в качестве второго аргумента
 // на выходе должен выходить массив элементов - результатов применения функции к каждому из элементов изначального массива
 
-//1-ый способ
+
 let array = [1, 2, 3, 4, 5];
+
+let multiplyByThree = (item) => {
+	return item * 3;
+};
 
 let mapAnArray = (arr) => {
 	let newArray = [];
+
 	arr.forEach((item) => {
-		newArray.push(item * 3);
+		newArray.push(multiplyByThree(item));
 	});
 	return newArray;
-};
-
-let mapFromScratch = (funk, array) => {
-	return funk(array);
-};
-console.log(mapFromScratch(mapAnArray, array));
-
-
-//2-ой способ
-let array = [1, 2, 3, 4, 5];
-
-let mapAnArray = (arr) => {
-	let newArr = arr.map((item) => item * 3);
-	return newArr;
 };
 
 let mapFromScratch = (funk, array) => {
