f2e1fecabc840db9ef67d95327e6991801dc23a2
Zhenya Aleksandrova <laberfo@gmail.com>
2022-02-03 23:24:22 +0300

    feature: added task 7 from HW2 (merge arrays in array to one array) - file mergeIntoOneArray_task9.ts

---
+++ mergeIntoOneArray_task9.ts
@@ -0,0 +1,42 @@
+// Напишите функцию, которая принимает массив массивов и соединяет их в один массив, у которого есть все элементы входных массивов.
+// Ваш код
+// → [1, 2, 3, 4, 5, 6]
+
+// 1 - first way
+
+let arrays: number[][] = [[1, 2, 3], [4, 5], [6]];
+
+const mergeIntoOneArray = (array: number[][]): number[] => {
+	let newArray: number[] = array.flat();
+
+	return newArray;
+};
+
+console.log(mergeIntoOneArray(arrays)); // → [1, 2, 3, 4, 5, 6]
+
+// 2 - second way
+
+let arrays2: number[][] = [[1, 2, 3], [4, 5], [6]];
+
+const mergeIntoOneArray2 = (array: number[][]): number[] => {
+	let newArray: number[] = array.reduce(
+		(accumulator, value) => accumulator.concat(value),
+		[]
+	);
+
+	return newArray;
+};
+
+console.log(mergeIntoOneArray2(arrays2)); // → [1, 2, 3, 4, 5, 6]
+
+// 3 - third way
+
+let arrays3: number[][] = [[1, 2, 3], [4, 5], [6]];
+
+const mergeIntoOneArray3 = (array: number[][]): number[] => {
+	let newArray: number[] = [].concat(...array);
+
+	return newArray;
+};
+
+console.log(mergeIntoOneArray3(arrays3)); // → [1, 2, 3, 4, 5, 6]
