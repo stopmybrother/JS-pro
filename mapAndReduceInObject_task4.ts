0b37f7d8a289ba4b09bd7c3b1d97f23467f7cada
Zhenya Aleksandrova <laberfo@gmail.com>
2022-02-09 21:22:30 +0300

    fix: get an array of object mapAndReduceInObject_task4.ts

--- mapAndReduceInObject_task4.ts
+++ mapAndReduceInObject_task4.ts
@@ -1,13 +1,19 @@
 // У нас есть объект, в котором хранятся зарплаты нашей команды.
 
+interface keyValue2 {
+	John: number;
+	Ann: number;
+	Pete: number;
+}
+
 // а) при помощи метода reduce найдите сумму зп
-let salaries: object = {
+let salaries: keyValue2 = {
 	John: 100,
 	Ann: 160,
 	Pete: 130,
 };
 
-const getSumOfSalaries = (obj: object): number => {
+const getSumOfSalaries = (obj: keyValue2): number => {
 	let arrOfSalaries: number[] = Object.values(obj); // [100, 160, 130]
 
 	let sumOfSalaries: number = arrOfSalaries.reduce(
@@ -20,18 +26,37 @@ const getSumOfSalaries = (obj: object): number => {
 console.log(getSumOfSalaries(salaries)); // 390
 
 // б) при помощи метода map дайте каждому участнику id (для этого можете использовать index)
-let salaries2: object = {
+interface keyValue6 {
+    John: number;
+    Ann: number;
+    Pete: number;
+}
+
+interface keyValue7 {
+    name: string;
+    salary: number;
+    id: number;
+}
+let salaries: keyValue6 = {
 	John: 100,
 	Ann: 160,
 	Pete: 130,
 };
 
-const getNewObjectWithId = (obj: object): object => {
-	let objOfId: object = Object.fromEntries(
-		Object.entries(obj).map(([key, value], index) => [key + 'Id', value = index + 1])
+const getArray = (obj: keyValue6): void => {
+	let counter: number = 0;
+	let arr = Object.entries(obj).map((item) => {
+		counter = counter + 1;
 
-	);
-	return objOfId;
+		let obj: keyValue7 = {
+			name: item[0],
+			salary: item[1],
+			id: counter,
+		};
+		item = obj;
+		return item;
+	});
+
+	console.log(arr);
 };
-
-console.log(getNewObjectWithId(salaries2)); // return new object with Id - { JohnId: 1, AnnId: 2, PeteId: 3 }
+console.log(getArray(salaries)); // [{name: John, salary: 100, id: 1}, {name: Ann, salary: 160, id: 2}}, {name: Pete, salary: 130, id: 3}]
