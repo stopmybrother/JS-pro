7283d674dc15510e3cbcbc5b3142f65b696148e6
Zhenya Aleksandrova <laberfo@gmail.com>
2022-02-11 21:59:58 +0300

    fix: change names of interfaces, added type of array, changed task b) with map in mapAndReduceInObject_task4.ts

--- mapAndReduceInObject_task4.ts
+++ mapAndReduceInObject_task4.ts
@@ -1,19 +1,30 @@
 // У нас есть объект, в котором хранятся зарплаты нашей команды.
 
-interface keyValue2 {
+interface IObjSalaries {
 	John: number;
 	Ann: number;
 	Pete: number;
 }
+interface IObjEmployee {
+	name: string;
+	salary: number;
+	id: number;
+}
 
-// а) при помощи метода reduce найдите сумму зп
-let salaries: keyValue2 = {
+type TArray = [
+    firstValue: string,
+    secondValue: number
+]
+
+let salaries1: IObjSalaries = {
 	John: 100,
 	Ann: 160,
 	Pete: 130,
 };
 
-const getSumOfSalaries = (obj: keyValue2): number => {
+// а) при помощи метода reduce найдите сумму зп
+
+const getSumOfSalaries = (obj: IObjSalaries): number => {
 	let arrOfSalaries: number[] = Object.values(obj); // [100, 160, 130]
 
 	let sumOfSalaries: number = arrOfSalaries.reduce(
@@ -23,40 +34,28 @@ const getSumOfSalaries = (obj: keyValue2): number => {
 	return sumOfSalaries;
 };
 
-console.log(getSumOfSalaries(salaries)); // 390
+console.log(getSumOfSalaries(salaries1)); // 390
 
 // б) при помощи метода map дайте каждому участнику id (для этого можете использовать index)
-interface keyValue6 {
-    John: number;
-    Ann: number;
-    Pete: number;
-}
 
-interface keyValue7 {
-    name: string;
-    salary: number;
-    id: number;
-}
-let salaries: keyValue6 = {
-	John: 100,
-	Ann: 160,
-	Pete: 130,
-};
+const getArray = (obj: IObjSalaries): void => {
 
-const getArray = (obj: keyValue6): void => {
-	let counter: number = 0;
-	let arr = Object.entries(obj).map((item) => {
-		counter = counter + 1;
+	let arr = Object.entries(obj).map(([key, value]: TArray, index: number): IObjEmployee[] => {
+        let newArr: IObjEmployee[] = [];
 
-		let obj: keyValue7 = {
-			name: item[0],
-			salary: item[1],
-			id: counter,
+		let obj: IObjEmployee = {
+			name: key,
+			salary: value,
+			id: index + 1,
 		};
-		item = obj;
-		return item;
+
+        if (obj) {
+            newArr.push(obj);
+        }
+		return newArr;
 	});
 
 	console.log(arr);
 };
-console.log(getArray(salaries)); // [{name: John, salary: 100, id: 1}, {name: Ann, salary: 160, id: 2}}, {name: Pete, salary: 130, id: 3}]
+
+console.log(getArray(salaries1)); // [{name: John, salary: 100, id: 1}, {name: Ann, salary: 160, id: 2}}, {name: Pete, salary: 130, id: 3}]
