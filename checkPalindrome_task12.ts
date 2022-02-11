775a971564326ea371ab84a0153ce9cf1ce0e2a0
Zhenya Aleksandrova <laberfo@gmail.com>
2022-02-11 22:09:03 +0300

    fix: used !! and Boolean (understood) in checkPalindrome_task12.ts

--- checkPalindrome_task12.ts
+++ checkPalindrome_task12.ts
@@ -1,10 +1,12 @@
 // 2) написать функцию, которая позволяет узнать, является ли строка полиндромом или же нет (читается одинаково с двух сторон)
 
-const str1: string = "шалаш";
-const str2: string = "казак";
-const str3: string = "слон";
+const str11: string = "шалаш";
+const str22: string = "казак";
+const str33: string = "слон";
 
-const checkPalindrome = (str: string): boolean => {
+// 1- first way
+
+const checkPalindrome1 = (str: string): boolean => {
 	let reverseString: string = str.split("").reverse().join("");
 	if (str === reverseString) {
 		return true;
@@ -13,6 +15,28 @@ const checkPalindrome = (str: string): boolean => {
 	}
 };
 
-console.log(checkPalindrome(str1)); // true
-console.log(checkPalindrome(str2)); // true
-console.log(checkPalindrome(str3)); // false
+console.log(checkPalindrome1(str11)); // true
+console.log(checkPalindrome1(str22)); // true
+console.log(checkPalindrome1(str33)); // false
+
+// 2 - second way
+
+const checkPalindrome2 = (str: string): boolean => {
+	let reverseString: string = str.split("").reverse().join("");
+	return !!(str === reverseString);
+};
+
+console.log(checkPalindrome2(str11)); // true
+console.log(checkPalindrome2(str22)); // true
+console.log(checkPalindrome2(str33)); // false
+
+// 3 - third way
+
+const checkPalindrome3 = (str: string): boolean => {
+	let reverseString: string = str.split("").reverse().join("");
+	return Boolean(str === reverseString);
+};
+
+console.log(checkPalindrome3(str11)); // true
+console.log(checkPalindrome3(str22)); // true
+console.log(checkPalindrome3(str33)); // false
