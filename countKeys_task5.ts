7c73c9b546b0176733ad6c5a5b1c2906acb54120
Zhenya Aleksandrova <laberfo@gmail.com>
2022-02-11 22:01:25 +0300

    fix: change names of interfaces, added third way with reduce-method in countKeys_task5.ts

--- countKeys_task5.ts
+++ countKeys_task5.ts
@@ -1,34 +1,36 @@
 // Напишите функцию count(obj), которая принимает объект и возвращает количество свойств объекта:
 
-interface keyValue3 {
+interface IObjPerson {
 	name: string;
 	age: number;
 	gender?: string;
 }
 
-// 1 - first way
-
-let user1: keyValue3 = {
+let user1: IObjPerson = {
 	name: "John",
 	age: 30,
 };
-
-let countKeys1 = (obj: keyValue3): number => {
-	let numberOfKeys: number = Object.keys(obj).length;
-	return numberOfKeys;
-};
-
-console.log(countKeys1(user1)); // 2
-
-// 2 - second way
-
-let user2: keyValue3 = {
+let user2: IObjPerson = {
 	name: "John",
 	age: 30,
 	gender: 'male'
 };
 
-const countKeys2 = (obj: keyValue3): number => {
+// 1 - first way
+
+
+let countKeys1 = (obj: IObjPerson): number => {
+	let numberOfKeys: number = Object.keys(obj).length;
+	return numberOfKeys;
+};
+
+console.log(countKeys1(user1)); // 2
+console.log(countKeys1(user2)); // 3
+
+// 2 - second way
+
+
+const countKeys2 = (obj: IObjPerson): number => {
     let counter: number = 0;
 	for (let key in obj) {
 		counter = counter + 1;
@@ -36,4 +38,24 @@ const countKeys2 = (obj: keyValue3): number => {
     return counter;
 };
 
-console.log(countKeys2(user2)); // 3
+console.log(countKeys1(user1)); // 2
+console.log(countKeys1(user2)); // 3
+
+// 3 - third way - reduce
+
+let countKeys3 = (obj: IObjPerson): number => {
+    let array: string[] = Object.keys(obj);
+
+    let numberOfKeys: number = array.reduce((prev: number, curr: string, index: number): number => {
+        if (curr[index]) {
+            prev = prev + 1;
+            return prev;
+        } else {
+            return 0;
+        }
+    }, 0)
+    return numberOfKeys;
+};
+
+console.log(countKeys1(user1)); // 2
+console.log(countKeys1(user2)); // 3
