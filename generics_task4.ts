6f06ed24c56fd008bec61c424a404f6f4a20278f
Zhenya Aleksandrova <laberfo@gmail.com>
2022-02-17 23:42:21 +0300

    feature: added generics_task4.ts

---
+++ generics_task4.ts
@@ -0,0 +1,13 @@
+//4) написать функцию, которая возвращает случайный элемент из переданного массива
+type TNumber = number;
+type TSring = string;
+
+const array1: TNumber[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const array2: TSring[] = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
+
+function getRandomElement<A, B>(arr: A[] | B[]): A | B {
+    let randomElement: number = Math.floor(Math.random() * arr.length);
+    return arr[randomElement];
+};
+console.log(getRandomElement<TNumber, TSring>(array1));
+console.log(getRandomElement<TNumber, TSring>(array2));
