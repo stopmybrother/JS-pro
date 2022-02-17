9e7bb6435cbbc220b096be2a21a497faaa0d5fda
Zhenya Aleksandrova <laberfo@gmail.com>
2022-02-17 23:35:40 +0300

    fix: add enum and const 18 and 65 + error with get-method - classInheritance_task14.ts

--- classInheritance_task14.ts
+++ classInheritance_task14.ts
@@ -1,8 +1,15 @@
 // Напишите родительский класс Person с обязательными полями name, age, и необязательным gender, метод printInfo, который выводит информацию о персоне в консоль. Класс Person сделайте абстрактным (см презентацию и доп материалы). 
+enum Gender {
+    male,
+    female,
+}
+
 abstract class Person {
     abstract name: string;
     abstract age: number;
-    abstract gender?: string | undefined;
+    abstract gender?: Gender;
+    static ageOfGraduation: number = 18;
+    static ageOfRetirement: number = 65;
 
     abstract printInfo(): void;
 }
@@ -11,21 +18,22 @@ abstract class Person {
 class Child extends Person {
     name: string;
     age: number;
-    gender?: string | undefined;
+    gender?: Gender;
     school: string;
-    constructor (name: string, age: number, school: string, gender?: string | undefined) {
+    constructor (name: string, age: number, school: string, ageOfGraduation: number, gender?: Gender) {
         super();
         this.name = name;
         this.age = age;
         this.school = school;
         this.gender = gender;
+        Child.ageOfGraduation = ageOfGraduation;
     }
     printInfo(): void {
-        if (this.gender === "male") {
+        if (this.gender === Gender.male) {
             console.log(`My name's ${this.name} and I'm a boy. I'm ${this.age} years old.`);
             console.log(`I study at the ${this.school}.`);
         };
-        if (this.gender === "female") {
+        if (this.gender === Gender.female) {
             console.log(`My name's ${this.name} and I'm a woman. I'm ${this.age} years old.`);
             console.log(`I study at the ${this.school}.`);
         };
@@ -35,35 +43,36 @@ class Child extends Person {
         }
     }
     get getNumberOfYearsUntilGraduation(): number {
-        let numberOfYearsUntilGraduation = 18 - this.age;
+        let numberOfYearsUntilGraduation = Person.ageOfGraduation - this.age;
         return numberOfYearsUntilGraduation;
     }
 };
 
-let child: Person = new Child("Addy", 12, "PM-school", "male");
+let child: Person = new Child("Addy", 12, "PM-school", Gender.male);
 child.printInfo();
-console.log(child.getNumberOfYearsUntilGraduation) // ????
+console.log(child.getNumberOfYearsUntilGraduation) // ? Property 'getNumberOfYearsUntilGraduation' does not exist on type 'Person'.
 
 // Создайте от него дочерний класс Adult, который содержит поле work и метод printAdultInfo(), который выводит информацию про взрослого и кем он работает 2 разными полями в console.log(). Определите для него get метод, сколько ему осталось до пенсии (до 65 лет)
 
 class Adult extends Person {
     name: string;
     age: number;
-    gender?: string | undefined;
+    gender?: Gender;
     work: string;
-    constructor (name: string, age: number, work:string, gender?: string) {
+    constructor (name: string, age: number, work:string, ageOfRetirement: number, gender?: Gender) {
         super();
         this.name = name;
         this.age = age;
         this.work = work;
         this.gender = gender;
+        Adult.ageOfRetirement = ageOfRetirement;
     }
     printInfo(): void {
-        if (this.gender === "male") {
+        if (this.gender === Gender.male) {
             console.log(`My name's ${this.name} and I'm a man. I'm ${this.age} years old.`);
             console.log(`My profession is ${this.work}.`);
         };
-        if (this.gender === "female") {
+        if (this.gender === Gender.female) {
             console.log(`My name's ${this.name} and I'm a woman. I'm ${this.age} years old.`);
             console.log(`My profession is ${this.work}.`);
         };
@@ -72,10 +81,10 @@ class Adult extends Person {
         }
     }
     get getNumberOfYearsUntilRetirement(): number {
-        let numberOfYearsUntilRetirement = 65 - this.age;
+        let numberOfYearsUntilRetirement = Person.ageOfRetirement - this.age;
         return numberOfYearsUntilRetirement;
     }
 }
-let adult: Person = new Adult("Gregory", 42, "engeneer", "male");
+let adult: Person = new Adult("Gregory", 42, "engeneer", Gender.male);
 adult.printInfo();
-console.log(adult.getNumberOfYearsUntilRetirement) // ????
+console.log(adult.getNumberOfYearsUntilRetirement) // ? Property 'getNumberOfYearsUntilRetirement' does not exist on type 'Person'.
