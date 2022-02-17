2bdb5ec25a0303d7acf613963dbf91d87c4f3971
Zhenya Aleksandrova <laberfo@gmail.com>
2022-02-17 23:34:16 +0300

    fix: in object chenges age - classUser_task13.ts

--- classUser_task13.ts
+++ classUser_task13.ts
@@ -1,4 +1,5 @@
-//Напишите класс User, который содержит обязательное неизменяемое поле name, обязательное обычное поле age, метод print() - который выводит в консоль имя и возраст, метод happyBirthday, который увеличивает возраст на 1 год.
+//Напишите класс User, который содержит обязательное неизменяемое поле name, обязательное обычное поле age,
+//метод print() - который выводит в консоль имя и возраст, метод happyBirthday, который увеличивает возраст на 1 год.
 
 class User {
     readonly name: string;
@@ -7,14 +8,16 @@ class User {
 		this.name = userName;
 		this.age = userAge;
 	}
-	print() {
+	print(): void {
 		console.log(`My name is ${this.name} and I'm ${this.age} years old.`);
 	}
-	happyBirthday() {
-		console.log(`Tomorrow is my Birthday. I'll be ${this.age + 1} years old.`);
+	happyBirthday(): void {
+        this.age = this.age + 1;
+		console.log(`Today is my Birthday. Now I'm ${this.age} years old.`);
 	}
 }
 
 let user = new User("Latisha", 25);
 user.print(); // My name is Latisha and I'm 25 years old.
-user.happyBirthday(); // Tomorrow is my Birthday. I'll be 31 years old.
+user.happyBirthday(); // Today is my Birthday. Now I'm 26 years old.
+console.log(user); // User: {"name": "Latisha", "age": 26} 
