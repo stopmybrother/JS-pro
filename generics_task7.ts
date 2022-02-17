3ae0526942140496693c0bd8009007e9b944d15d
Zhenya Aleksandrova <laberfo@gmail.com>
2022-02-17 23:43:10 +0300

    feature: added generics_task7.ts

---
+++ generics_task7.ts
@@ -0,0 +1,32 @@
+//7) написать функцию, которая будет принимать объект и изменять значение его свойства 
+//при этом функция должна быть безопасной 
+//(т.е если тип данных у свойства number, то менять его можно только на number)
+type TArr = (number | string)[][]
+interface IObject {
+    name: string;
+    surname: string;
+    age: number;
+    gender: string;
+    children: boolean;
+};
+
+let object: IObject = {
+    name: "Patric",
+    surname: "Swon",
+    age: 24,
+    gender: "Male",
+    children: false,
+};
+
+function changeValue<T> (obj: T, key: string, value: string | number | boolean): T {
+    let arr: TArr = Object.entries(obj).map((item, index) => {
+        if (item[0] === key && typeof item[1] === typeof value) {
+            item[1] = value;
+        }
+        return item;
+    });
+    let newObj = Object.fromEntries(arr);
+    return newObj
+};
+console.log(changeValue<IObject>(object, "children", true)); // {"name": "Patric", "surname": "Swon", "age": 24, "gender": "Male", "children": true}
+console.log(changeValue<IObject>(object, "children", "true")); // {"name": "Patric", "surname": "Swon", "age": 24, "gender": "Male", "children": false}
