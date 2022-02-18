cfe6f15fde05be3063361b914f8e9f15c2594add
Zhenya Aleksandrova <laberfo@gmail.com>
2022-02-18 23:02:36 +0300

    feature: added letterCounter_extraTask1.ts with reduce ))

---
+++ letterCounter_extraTask1.ts
@@ -0,0 +1,15 @@
+//нужно написать функцию letterCounter, которая принимает в себя любое слово и как результат выводит объект, в котором каждая буква слово - ключ, а значение - количество повторов данной буквы в этом слове
+// например для слова "дата" это будет - { д: 1, а: 2, т: 1 } с помощью метода reduce
+interface IObj {
+    [key: string]: number;
+}
+const string1: string = "data";
+const string2: string = "ddaaaattta";
+
+const letterCounter = (str: string): IObj => {
+    return str.split("").reduce((prev: IObj, item: string) => {
+        return prev[item] ? prev[item]++ : prev[item] = 1, prev;
+    }, {})
+}
+console.log(letterCounter(string1)); // {"d": 1, "a": 5, "t": 2} 
+console.log(letterCounter(string2)); // {"d": 2, "a": 5, "t": 3} 
