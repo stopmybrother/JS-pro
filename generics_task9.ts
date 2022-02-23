21b0db63eeff7a04d959b344a05c979df33e81fc
Zhenya Aleksandrova <laberfo@gmail.com>
2022-02-23 21:39:29 +0300

    fix: remove extra-generics in generics_task9.ts - have questions

--- generics_task9.ts
+++ generics_task9.ts
@@ -20,11 +20,11 @@ let array: IOld[] = [
     }
 ];
 
-function pushKeyInObj<A, B>(array: A[]): B[] {
-    let newArray: B[] = [...array].map((item: A, index: number): B => {
+function pushKeyInObj<A>(array: A[]): INew[] {
+    let newArray: INew[] = [...array].map((item: A , index: number): INew => {
         item.id = index + 1;
         return item;
     })
     return newArray;
 }
-console.log(pushKeyInObj<IOld, INew>(array)); //[{"name": "Kenny", "id": 1}, {"name": "Brad", "id": 2}, {"name": "Pit", "id": 3}] 
+console.log(pushKeyInObj<IOld>(array)); //[{"name": "Kenny", "id": 1}, {"name": "Brad", "id": 2}, {"name": "Pit", "id": 3}] 
