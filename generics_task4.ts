dd1823bb171c051336a0a258e9af5a00e3210198
Zhenya Aleksandrova <laberfo@gmail.com>
2022-02-23 21:35:59 +0300

    fix: remove extra-generic in generics_task4.ts

--- generics_task4.ts
+++ generics_task4.ts
@@ -5,9 +5,9 @@ type TSring = string;
 const array1: TNumber[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const array2: TSring[] = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
 
-function getRandomElement<A, B>(arr: A[] | B[]): A | B {
+function getRandomElement<A>(arr: A[]): A {
     let randomElement: number = Math.floor(Math.random() * arr.length);
     return arr[randomElement];
 };
-console.log(getRandomElement<TNumber, TSring>(array1));
-console.log(getRandomElement<TNumber, TSring>(array2));
+console.log(getRandomElement<TNumber | TSring>(array1));
+console.log(getRandomElement<TNumber | TSring>(array2));
