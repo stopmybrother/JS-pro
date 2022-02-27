3a44c2add20c619b55dec53b0d3397baf2e7f376
Zhenya Aleksandrova <laberfo@gmail.com>
2022-02-27 22:11:06 +0300

    feature: added algorithmsAndDataStructures_task3.ts - get sum of other elements in array and map this array

---
+++ algorithmsAndDataStructures_task3.ts
@@ -0,0 +1,12 @@
+//задача 3
+//дан массив чисел. нужно написать функцию, которая вернет массив,
+//в котором на месте каждого из предыдущих чисел будет сумма всех оставшихся кроме него
+const arr = [3, 5, 2] // [7, 5, 8]
+
+const getNewArr = (array: number[]): number[] => {
+    let sum: number = [...array].reduce((acc, item) => acc + item, 0)
+    return [...array].map((item) => {
+        return sum - item;
+    })
+}
+console.log(getNewArr(arr)); // [7, 5, 8]
