902b672b3d4b82a937b2c10db4cd79913a1cc711
Zhenya Aleksandrova <laberfo@gmail.com>
2022-02-27 22:08:46 +0300

    feature: added algorithmsAndDataStructures_task1.ts - big O notation

---
+++ algorithmsAndDataStructures_task1.ts
@@ -0,0 +1,19 @@
+// задача 1
+//решить задачу минимум 2 способами и определить их временную сложность
+//Дан массив [1,5,87,6]. нужно написать функцию, которая будет складывать все элементы массива и возвращать сумму
+
+const array: number[] = [1, 5, 87, 6];
+
+const getSum1 = (array: number[]): number => {
+    return [...array].reduce((acc, item) => acc + item, 0); // 1
+}
+console.log(getSum1(array)); 
+
+const getSum2 = (array: number[]): number => {
+    let sum: number = 0; // 1
+    array.forEach((item) => {
+        sum += item; // 4
+    }); // 1
+    return sum; // 1
+}
+console.log(getSum2(array)); // 7
