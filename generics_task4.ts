f054e8f9604fe930f6128e9aeff7db0c1f3aac45
Zhenya Aleksandrova <laberfo@gmail.com>
2022-03-02 19:32:14 +0300

    fix: fix generics in generics_task.ts

--- generics_task4.ts
+++ generics_task4.ts
@@ -1,13 +1,10 @@
 //4) написать функцию, которая возвращает случайный элемент из переданного массива
-type TNumber = number;
-type TSring = string;
-
-const array1: TNumber[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const array2: TSring[] = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
+const array1: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const array2: string[] = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
 
 function getRandomElement<A>(arr: A[]): A {
     let randomElement: number = Math.floor(Math.random() * arr.length);
     return arr[randomElement];
 };
-console.log(getRandomElement<TNumber | TSring>(array1));
-console.log(getRandomElement<TNumber | TSring>(array2));
+console.log(getRandomElement<number>(array1));
+console.log(getRandomElement<string>(array2));
